d828d0f68f8f9b62f92afd0d8451ac338be4c3aa
SquaredCub <SquaredCub@users.noreply.github.com>
2024-01-28 05:12:08 +0100

    added better mobile support

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,11 @@ import {
 } from "./useSuitabilityFilters";
 import { type IPal, SUITABILITIES } from "./interfaces";
 import { LazyLoadImage } from "react-lazy-load-image-component";
+import { useState } from "react";
+import MenuButton from "./MenuButton";
 
 const App = () => {
+  const [menuOpen, setMenuOpen] = useState<boolean>(false);
   const {
     handleMinSliderChange,
     handleMaxSliderChange,
@@ -35,76 +38,88 @@ const App = () => {
     return doesThisPalRespectSuitabilityFilters;
   });
 
+  const toggleMenu = () => {
+    setMenuOpen((old) => !old);
+  };
+
   return (
     <main>
-      <section className="filters">
-        <div className="filters-title">
-          <h2>Filters</h2>
-          <button onClick={resetFilters}>Reset</button>
-        </div>
-        <div className="group">
-          <h3 className="group-title">Minimum Level</h3>
-          {SUITABILITIES.map((s) => (
-            <label htmlFor={s} key={`minSuitabilityFilter${s}`}>
-              <span>
-                <LazyLoadImage
-                  src={`/palfinder/images/suitabilities/${s.replace(
-                    " ",
-                    "_"
-                  )}.png`}
-                  width="30px"
-                  height="30px"
-                  title={`min ${s}`}
+      <section className={`filtersSection ${menuOpen ? "open" : "collapsed"}`}>
+        <div className="filters">
+          <div className="filters-title">
+            <MenuButton handleToggleMenu={toggleMenu} />
+            <h2>Filters</h2>
+            <button onClick={resetFilters}>Reset</button>
+          </div>
+          <div className="group">
+            <h3 className="group-title">Minimum Level</h3>
+            {SUITABILITIES.map((s) => (
+              <label htmlFor={s} key={`minSuitabilityFilter${s}`}>
+                <span>
+                  <LazyLoadImage
+                    src={`/palfinder/images/suitabilities/${s.replace(
+                      " ",
+                      "_"
+                    )}.png`}
+                    width="30px"
+                    height="30px"
+                    title={`min ${s}`}
+                  />
+                </span>
+                <input
+                  type="range"
+                  min="0"
+                  max="4"
+                  value={suitabilityFilters[s].min}
+                  className="slider"
+                  id={`min |${s}`}
+                  name={`min ${s}`}
+                  onChange={handleMinSliderChange}
                 />
-              </span>
-              <input
-                type="range"
-                min="0"
-                max="4"
-                value={suitabilityFilters[s].min}
-                className="slider"
-                id={`min |${s}`}
-                name={`min ${s}`}
-                onChange={handleMinSliderChange}
-              />
-              <span>{suitabilityFilters[s].min}</span>
-            </label>
-          ))}
-        </div>
-        <div className="group">
-          <h3 className="group-title">Maximum Level</h3>
-          {SUITABILITIES.map((s) => (
-            <label htmlFor={s} key={`maxSuitabilityFilter${s}`}>
-              <span>
-                <LazyLoadImage
-                  src={`/palfinder/images/suitabilities/${s.replace(
-                    " ",
-                    "_"
-                  )}.png`}
-                  width="30px"
-                  height="30px"
-                  title={`max ${s}`}
+                <span>{suitabilityFilters[s].min}</span>
+              </label>
+            ))}
+          </div>
+          <div className="group">
+            <h3 className="group-title">Maximum Level</h3>
+            {SUITABILITIES.map((s) => (
+              <label htmlFor={s} key={`maxSuitabilityFilter${s}`}>
+                <span>
+                  <LazyLoadImage
+                    src={`/palfinder/images/suitabilities/${s.replace(
+                      " ",
+                      "_"
+                    )}.png`}
+                    width="30px"
+                    height="30px"
+                    title={`max ${s}`}
+                  />
+                </span>
+                <input
+                  type="range"
+                  min="0"
+                  max="4"
+                  value={suitabilityFilters[s].max}
+                  className="slider"
+                  id={`max |${s}`}
+                  name={`max ${s}`}
+                  onChange={handleMaxSliderChange}
                 />
-              </span>
-              <input
-                type="range"
-                min="0"
-                max="4"
-                value={suitabilityFilters[s].max}
-                className="slider"
-                id={`max |${s}`}
-                name={`max ${s}`}
-                onChange={handleMaxSliderChange}
-              />
-              <span>{suitabilityFilters[s].max}</span>
-            </label>
-          ))}
+                <span>{suitabilityFilters[s].max}</span>
+              </label>
+            ))}
+          </div>
         </div>
       </section>
-      <section className="pals">
-        {pals.map((p) => (
-          <PalCard pal={p as IPal} key={p.id} />
-        ))}
+      <section className={`palsSection ${menuOpen ? "collapsed" : "open"}`}>
+        <div className="menuButton-wrapper">
+          <MenuButton handleToggleMenu={toggleMenu} />
+        </div>
+        <div className="pals">
+          {pals.map((p) => (
+            <PalCard pal={p as IPal} key={p.id} />
+          ))}
+        </div>
       </section>
     </main>
   );
